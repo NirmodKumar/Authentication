1f6dee69108fbf345dd59c71ae4dd9a0ada2bb64
Nirmod Kumar <nirmod.kumar656@gmail.com>
2023-10-21 17:47:56 +0530

    Added use of add authorization service

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 const string AuthScheme = "cookie";
 
@@ -8,26 +9,53 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(AuthScheme)
     .AddCookie(AuthScheme);
 
+builder.Services.AddAuthorization(auth =>
+{
+    auth.AddPolicy("passport_ind", pb =>
+    {
+        pb.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(AuthScheme)
+        .RequireClaim("passport_type", "ind");
+    });
+    auth.AddPolicy("passport_usa", pb =>
+    {
+        pb.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(AuthScheme)
+        .RequireClaim("passport_type", "usa");
+    });
+    auth.AddPolicy("passport_uae", pb =>
+    {
+        pb.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(AuthScheme)
+        .RequireClaim("passport_type", "uae");
+    });
+
+    auth.AddPolicy("Custom-Authorization-Workflow", pb =>
+    {
+        pb.RequireAuthenticatedUser();
+    });
+});
+
 var app = builder.Build();
 
 app.UseAuthentication();
 
 app.Use((ctx, next) =>
 {
-    if (ctx.Request.Path.StartsWithSegments("/login"))
-    {
-        return next();
-    }
-    if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
-    {
-        ctx.Response.StatusCode = 401;
-        return Task.CompletedTask;
-    }
-    if (!ctx.User.HasClaim("passport_type", "ind"))
-    {
-        ctx.Response.StatusCode = 403;
-        return Task.CompletedTask;
-    }
+    //if (ctx.Request.Path.StartsWithSegments("/login"))
+    //{
+    //    return next();
+    //}
+    //if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
+    //{
+    //    ctx.Response.StatusCode = 401;
+    //    return Task.CompletedTask;
+    //}
+    //if (!ctx.User.HasClaim("passport_type", "ind"))
+    //{
+    //    ctx.Response.StatusCode = 403;
+    //    return Task.CompletedTask;
+    //}
 
     return next();
 });
@@ -36,55 +64,22 @@ app.MapGet("/unsecure", (HttpContext ctx) =>
 {
     var usr = ctx.User.FindFirst("usr")?.Value ?? "empty";
     return usr;
-});
+}).RequireAuthorization("passport_ind");
 
 app.MapGet("/india", (HttpContext ctx) =>
 {
-    //if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
-    //{
-    //    ctx.Response.StatusCode = 401;
-    //    return "";
-    //}
-    //if (!ctx.User.HasClaim("passport_type", "ind"))
-    //{
-    //    ctx.Response.StatusCode = 403;
-    //    return "";
-    //}
-
     return "allowed";
-});
+}).RequireAuthorization("passport_ind");
 
 app.MapGet("/usa", (HttpContext ctx) =>
 {
-    //if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
-    //{
-    //    ctx.Response.StatusCode = 401;
-    //    return "";
-    //}
-    //if (!ctx.User.HasClaim("passport_type", "usa"))
-    //{
-    //    ctx.Response.StatusCode = 403;
-    //    return "";
-    //}
-
     return "allowed";
-});
+}).RequireAuthorization("passport_usa"); ;
 
 app.MapGet("/uae", (HttpContext ctx) =>
 {
-    //if (!ctx.User.Identities.Any(x => x.AuthenticationType == AuthScheme))
-    //{
-    //    ctx.Response.StatusCode = 401;
-    //    return "";
-    //}
-    //if (!ctx.User.HasClaim("passport_type", "uae"))
-    //{
-    //    ctx.Response.StatusCode = 403;
-    //    return "";
-    //}
-
     return "allowed";
-});
+}).RequireAuthorization("passport_uae"); ;
 
 app.MapGet("/login", async (HttpContext ctx) =>
 {
@@ -95,12 +90,33 @@ app.MapGet("/login", async (HttpContext ctx) =>
     var usr = new ClaimsPrincipal(identity);
     await ctx.SignInAsync(AuthScheme, usr);
     return "SignIn";
-});
+}).AllowAnonymous();
 
 app.MapGet("/logout", async (HttpContext ctx) =>
 {
     await ctx.SignOutAsync(AuthScheme);
     return "SignOut";
-});
+}).AllowAnonymous();
 
 app.Run();
+
+
+public class MyAuthorizationRequirement : IAuthorizationRequirement
+{
+    public MyAuthorizationRequirement()
+    {
+    }
+}
+
+public class MyRequirementHandler : AuthorizationHandler<MyAuthorizationRequirement>
+{
+    public MyRequirementHandler()
+    {
+
+    }
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MyAuthorizationRequirement requirement)
+    {
+
+        return Task.CompletedTask;
+    }
+}
